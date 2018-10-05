23c2d761e6638f884c340d57ecff4e0e497b10bd
enmalafeev <malafeev.en@yandex.ru>
2018-10-05 20:27:26 +0300

    fix maxNum and minNum in gcd

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -9,21 +9,15 @@ const minNumber = 1;
 const maxNumber = 10;
 
 const gcd = (a, b) => {
-  let minNum = 0;
-  let maxNum = 0;
-  if (a > b) {
-    maxNum = a;
-    minNum = b;
-  }
-  maxNum = b;
-  minNum = a;
+  const minNum = Math.min(a, b);
+  const maxNum = Math.max(a, b);
   const acc = [];
   for (let i = 1; i <= minNum; i += 1) {
     if (minNum % i === 0 && maxNum % i === 0) {
       acc.push(i);
     }
   }
-  return acc.sort((a, b) => b - a)[0];
+  return acc.sort((x, y) => y - x)[0];
 };
 
 const generateQuestion = () => {
