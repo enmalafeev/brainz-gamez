bd65b8a1c73a37bab1347a8923459d275215a6ea
enmalafeev <malafeev.en@yandex.ru>
2018-10-05 14:03:34 +0300

    rename gameQuestion

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import { car, cdr } from 'hexlet-pairs';
 
 const attemptCount = 3;
 
-const startGame = (description, question) => {
+const startGame = (description, gameQuestion) => {
   console.log('Welcome to the Brain Games!');
   console.log(description);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
   for (let counter = 0; counter < attemptCount; counter += 1) {
-    const getGameQuestion = question();
-    const gameQuestion = car(getGameQuestion);
-    console.log(`Question: ${gameQuestion}`);
+    const getGameQuestion = gameQuestion();
+    const question = car(getGameQuestion);
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const rightAnswer = cdr(getGameQuestion);
     if (userAnswer === rightAnswer) {
